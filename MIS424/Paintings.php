74794384f589aa3297bab016f2f446f4c61cc584
czacarias905 <60229233+czacarias905@users.noreply.github.com>
2021-01-19 18:50:22 -0600

    Add files via upload

---
+++ MIS424/Paintings.php
@@ -0,0 +1,68 @@
+<!-- Load Font Awesome Icon Library -->
+
+<head>
+<link rel="stylesheet" href="font-awesome.min.css">
+
+<link href="paintingpage.css" rel="stylesheet" type="text/css">
+
+<link href="css%20defualt.css" rel="stylesheet" type="text/css">
+
+<!-- Buttons to choose list or grid view -->
+</head>
+
+<button onclick="listView()"><i class="fa fa-bars"></i> List</button>
+<button onclick="gridView()"><i class="fa fa-th-large"></i> Grid</button>
+
+<div class="row">
+  <div class="column" style="background-color:#FFFFCC;">
+    <h2>Paris Cityscape 1</h2>
+    <p>
+   			<th class="auto-style1" style="width: 916; height: 896px;">
+			<img alt="Paris cityscape" class="auto-style5" height="643" src="Photos/PXL_20201021_174046733.jpg" style="float: right" width="500"></th>
+			<td style="width: 832px; height: 896px;" class="auto-style1">
+			<input type="submit" value="Submit"
+			</p>
+  </div>
+ 
+<div class="column" style="background-color:#FFFFCC;">
+    <h2>Chicago Cityscape</h2>
+    <p><img alt="Chicago cityscape" height="639" src="Photos/PXL_20201021_174227397.jpg" style="float: right" width="500"></td>
+		<td style="width: 916px; height: 854px;" class="auto-style1">
+		<input type="submit" value="Submit">
+    </p>
+  </div>
+</div>
+
+<div class="row">
+  <div class="column" style="background-color:#FFFFCC;">
+    <h2>Barcelona Cityscape</h2>
+    <p><img alt="Barcelona cityscape" height="681" src="Photos/PXL_20201021_174330046.jpg" style="float: right" width="500"></td>
+		<td style="width: 832px; height: 883px;" class="auto-style1">
+						<input type="submit" value="Submit">
+</p>
+</div>
+  <div class="column" style="background-color:#FFFFCC;">
+    <h2>Blossoms</h2>
+    <p><img alt="Blossoms" height="629" src="Photos/PXL_20201021_174609987.jpg" style="float: right" width="400"></td>
+		<td style="width: 832px; height: 883px;" class="auto-style1">
+	<input type="submit" value="Submit"></p>
+  </div>
+</div>
+
+  <div class="column" style="background-color:#FFFFCC;">
+    <h2>Flowers for you</h2>
+    <p><img alt="Flowers for you" height="484" src="Photos/PXL_20201021_174924237.jpg" style="float: right" width="500"></td>
+		<input type="submit" value="Submit">
+    </p>
+</p>
+  </div>
+  <div class="column" style="background-color:#FFFFCC;">
+    <h2>I love</h2>
+    <p><img alt="I love" height="640" src="PXL_20201021_174513853.jpg" style="float: right" width="400">&nbsp;</td>
+		<input type="submit" value="Submit">
+    </p>
+</p>
+  </div>
+
+
+<?php include('file:///C:/Users/czaca/OneDrive/Documents/NLU/MIS424/paintingpage.js'); ?>
